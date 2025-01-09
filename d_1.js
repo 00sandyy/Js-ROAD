ab7d9ada9f417382f58ce80b792ce248d98073a1
Sandip Gyawali <sandipis111@gmail.com>
2025-01-09 14:13:36 +0545

    firrst commit

---
+++ d_1.js
@@ -0,0 +1,25 @@
+
+
+// console.log("Hello World");
+// // Output: Hello World
+// // Explanation: The code snippet is a simple console.log statement that prints Hello World to the console.
+// //Arithmetic Operators
+// let a=5;
+// let b=2;
+// console.log("a=",a,"b=",b);
+// console.log("a+b=",a+b); //7
+// console.log("a-b=",a-b); //3
+// console.log("a*b=",a*b); //10
+// console.log("a/b=",a/b); //2.5
+// console.log("a%b=",a%b); //1
+// console.log("a**b=",a**b); //25
+// a++;
+// console.log(a);//6
+
+// b--;
+// console.log(b);//1
+
+let a=12,b=5;
+b**=2;
+console.log(b);
+
